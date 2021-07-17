a083581bfb5b9894ecdf91a2a0349d88af9ee4fa
Mark Pieciak <mpieciak18@msn.com>
2021-07-16 21:54:10 -0400

    refactor script.js to use async & await

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const image = document.getElementById('image');
 const overlay = document.getElementById('image-overlay');
 
-const fetchGif = function(keyword) {
+const fetchGif = async function(keyword) {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=3dm9zfS3BFtWQZVB9gF7PvgbXEU4GkMH&q=${keyword}`;
-    
-    fetch(url, {mode: 'cors'})
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        const randomResult = response.data[Math.floor(Math.random() * response.data.length)];
+    try {
+        const response = await fetch(url, {mode: 'cors'});
+        const results = await response.json();
+        const randomResult = results.data[Math.floor(Math.random() * results.data.length)];
+
         if (typeof randomResult != "undefined") {
             image.onload = () => {overlay.innerHTML = ''};
             image.src = randomResult.images.original.url;
@@ -17,11 +15,10 @@ const fetchGif = function(keyword) {
             image.onload = () => {overlay.innerHTML = 'Hmm...<br>nothing found.'};
             image.src = "https://media3.giphy.com/media/8GTKaetBL5IVBMr18A/giphy.gif?cid=790b761134632c364317a9a695e545772dfea6dd6438cc88&rid=giphy.gif&ct=g";
         };
-    })
-    .catch((error) => {
+    } catch(error) {
         image.onload = () => {overlay.innerHTML = 'Hmm...<br>nothing found.'};
         image.src = "https://media3.giphy.com/media/8GTKaetBL5IVBMr18A/giphy.gif?cid=790b761134632c364317a9a695e545772dfea6dd6438cc88&rid=giphy.gif&ct=g";
-    });
+    };
 };
 
 const searchGif = function(event) {
